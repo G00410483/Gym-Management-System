bf334cbaf9aaa1242eb20a3599aca1e8172adadf
David V <dvlasic000@gmail.com>
2024-02-26 14:30:53 +0000

    Adding card display functionallity to Classes Page

--- src/components/ClassesPage.js
+++ src/components/ClassesPage.js
@@ -15,13 +15,8 @@ function ClassesPage() {
 
   const generateTableData = (timeIndex) => {
     return daysOfWeek.map((_, dayIndex) => (
-      <td
-        key={`time-${timeIndex}-day-${dayIndex}`}
-        className={selectedClass === `Class ${dayIndex + 1}-${timeIndex + 1}` ? 'selected' : ''}
-      >
-        <button onClick={() => handleClassSelect(dayIndex, timeIndex)}>
-          Class {dayIndex + 1}-{timeIndex + 1}
-        </button>
+      <td key={`time-${timeIndex}-day-${dayIndex}`} className={selectedClass === `Class ${dayIndex + 1}-${timeIndex + 1}` ? 'selected' : ''}>
+        <Button onClick={() => handleClassSelect(dayIndex, timeIndex)}>Class {dayIndex + 1}-{timeIndex + 1}</Button>
       </td>
     ));
   };
@@ -47,21 +42,22 @@ function ClassesPage() {
           ))}
         </tbody>
       </table>
-      <div>
-        <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>{selectedClass && <p>Selected Class: {selectedClass}</p>}</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
+      {selectedClass && (
+      <div className="card-container">
+        <Card className="card-style">
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Selected Class: {selectedClass}</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
       </div>
-    </div>
-  );
+    )}
+  </div>
+);
 }
 
 export default ClassesPage;
